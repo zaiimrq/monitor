14750c4692485de91407cdd61e2640cf37855f17
zaiimrq <zaiimrq@gmail.com>
2024-05-20 21:19:01 +0900

    filament user

--- database/migrations/2024_05_20_022033_add_role_column_to_users.php
+++ database/migrations/2024_05_20_022033_add_role_column_to_users.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enums\Role;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->addColumn('string', 'role')->default('team-sukses');
+            $table->addColumn('string', 'role')->default(Role::Admin->value);
         });
     }
 
